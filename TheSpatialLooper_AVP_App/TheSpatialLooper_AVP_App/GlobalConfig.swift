871b9b2ba72d13896c573fbc387bcba709109551
SerialNo25 <flurin.selm@student.unisg.ch>
2025-04-08 17:00:42 +0200

    feat[device-configuration]: configured implemented synthesizer bounding boxes

--- TheSpatialLooper_AVP_App/TheSpatialLooper_AVP_App/GlobalConfig.swift
+++ TheSpatialLooper_AVP_App/TheSpatialLooper_AVP_App/GlobalConfig.swift
@@ -10,7 +10,7 @@ import Foundation
 enum GlobalConfig {
     // MARK: - DEBUG MODES
     static let SHOW_HAND_TRACKING_JOINTS = false
-    static let SHOW_BOUNDING_BOXES = false
+    static let SHOW_BOUNDING_BOXES = true
     
     // MARK: - SESSION CONFIG
     static let LOOP_GRID_WIDTH: Int = 5
@@ -19,7 +19,11 @@ enum GlobalConfig {
     // MARK: - TRACK CONFIG
     // NOTE: The Reference Object Name is mapped to the tracker AND filename. It is required that the name AND filename match the internal name of the tracker.
     static let LOOP_SOURCE_CONFIGURATIONS: [LoopSourceConfiguration] = [
-        LoopSourceConfiguration(sourceName: "TestSource", trackID: 0, boundingBoxX: 1, boundingBoxY: 0.1, boundingBoxZ: 0.5, boundingBoxOffsetZ: 0.1, viewAttachmentVerticalOffset: 0.2, referenceObjectName: "TR-8S_Scaled")
+        LoopSourceConfiguration(sourceName: "TR-8S", trackID: 0, boundingBoxX: 0.40, boundingBoxY: 0.12, boundingBoxZ: 0.3, boundingBoxOffsetY: 0.04, boundingBoxOffsetZ: 0.1, viewAttachmentVerticalOffset: 0.2, referenceObjectName: "TR-8S_Scaled"),
+        LoopSourceConfiguration(sourceName: "TEO-5", trackID: 1, boundingBoxX: 0.64, boundingBoxY: 0.17, boundingBoxZ: 0.4, boundingBoxOffsetY: 0.07, boundingBoxOffsetZ: 0.08, viewAttachmentVerticalOffset: 0.2, referenceObjectName: "OberheimScaled"),
+        LoopSourceConfiguration(sourceName: "NordStage3", trackID: 2, boundingBoxX: 1.3, boundingBoxY: 0.17, boundingBoxZ: 0.3, boundingBoxOffsetX: 0.05, boundingBoxOffsetY: 0.15, boundingBoxOffsetZ: 0.22, viewAttachmentVerticalOffset: 0.2, referenceObjectName: "nord"),
+        LoopSourceConfiguration(sourceName: "Fantom06", trackID: 3, boundingBoxX: 1, boundingBoxY: 0.17, boundingBoxZ: 0.34, boundingBoxOffsetY: 0.08, boundingBoxOffsetZ: 0.1, viewAttachmentVerticalOffset: 0.2, referenceObjectName: "Fantom"),
+        LoopSourceConfiguration(sourceName: "MS-20", trackID: 4, boundingBoxX: 0.6, boundingBoxY: 0.3, boundingBoxZ: 0.3, boundingBoxOffsetY: 0.23, boundingBoxOffsetZ: 0.15, viewAttachmentVerticalOffset: 0.2, referenceObjectName: "MS-20")
     ]
     
     // MARK: - CONTROL CONFIG
